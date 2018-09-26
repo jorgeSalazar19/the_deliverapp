fca5017e6eaebf9cf751313714e7d5c51913e34d
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2018-09-26 15:54:33 -0500

    agregando plantillas

--- deliveries/views.py
+++ deliveries/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def Index(request):
+    template = loader.get_template('landing.html')
+    ctx = {}
+    return HttpResponse(template.render(ctx,request))
