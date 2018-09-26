6da27df89e8e61c0a29e064288b739219bc1f189
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2018-09-26 17:56:49 -0500

    pull

--- the_deliverapp/settings.py
+++ the_deliverapp/settings.py
@@ -132,12 +132,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join('/home/jorge/Documentos/the_deliverapp', 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join('/home/jorge/Documentos/the_deliverapp', 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
