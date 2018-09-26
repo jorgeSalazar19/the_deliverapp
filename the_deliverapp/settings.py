c2b0221ca95509f1427ef38936040a35edb2aff1
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2018-09-26 18:15:31 -0500

    add static for landing page

--- the_deliverapp/settings.py
+++ the_deliverapp/settings.py
@@ -128,16 +128,16 @@ USE_TZ = True
 
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join('/home/jorge/Documentos/the_deliverapp', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join('/home/jorge/Documentos/the_deliverapp', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
