01db094cffcf515aeeef87934ea4aed070e62a50
Ivan Nieves Ardila <gykour.friki.gamer@gmail.com>
2018-09-27 22:45:39 -0500

    landing no hice casi nada

--- the_deliverapp/settings.py
+++ the_deliverapp/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
          }
  }
 
+
 db_from_env = dj_database_url.config(conn_max_age=500)  
 DATABASES['default'].update(db_from_env)
 
@@ -128,16 +129,14 @@ USE_TZ = True
 
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join('/home/jorge/Documentos/the_deliverapp', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join('/home/jorge/Documentos/the_deliverapp', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
